7a1b28ad1215ab963791e3776d8e5c7f26884a77
toxidity-18 <desavigatunganga@gmail.com>
2024-09-01 09:07:24 +0300

    first

---
+++ lib/navigationpytest.py
@@ -0,0 +1,3 @@
+# pytest lib/testing
+# pytest lib/testing/subdirectory/bool_test.py
+# pytest lib/testing/subdirectory/bool_test.py::test_return_true
---
+++ lib/tdd.py
@@ -0,0 +1,4 @@
+def interpolate_welcome(name):
+    pass
+    assert interpolate_welcome('Guido') == 'Welcome, Guido!'
+    print ()
